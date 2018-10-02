f11e95cdec1477ffeff9b605ecf3ec6a471f5e7a
Ron Craig <ron.craig@comcast.net>
2018-10-02 15:20:21 -0700

    edit specs to focus on a word, not the entire haiku

--- spec/haiku-checker-spec.js
+++ spec/haiku-checker-spec.js
@@ -2,49 +2,48 @@ let Solution = require('./../src/haiku-checker.js').solutionModule;
 
 describe('Solution', function() {
   it('should count number of words in the haiku', function() {
-    let goodHaiku = "An old silent pond, A frog jumps into the pond, splash! Silence again.";
-    let wordSolution = new Solution(goodHaiku);
+    let wordSolution = new Solution("An old silent pond, A frog jumps into the pond, splash! Silence again.");
     expect(wordSolution.countWords()).toEqual(13);
   });
 
-  it('should be able to detect 2 or more consonants next to each other', function () {
-    let consonantsSolution = new Solution("An old silent pond, A frog jumps into the pond, splash! Silence again.");
-    expect(consonantsSolution.countNextConsonants()).toEqual(11);
+  it('should be able to detect 2 or more consonants for a word', function () {
+    let consonantsSolution = new Solution("jumps");
+    expect(consonantsSolution.countNextConsonants()).toEqual(1);
   });
 
-  it('should be able to count vowels for each word', function () {
-    let consonantsSolution = new Solution("An old silent pond, A frog jumps into the pond, splash! Silence again.");
-    expect(consonantsSolution.countVowels()).toEqual(19);
+  it('should be able to count vowels for a word', function () {
+    let countVowelsSolution = new Solution("Silence");
+    expect(countVowelsSolution.countVowels()).toEqual(3);
   });
 
-  it('should be able to count vowels and consonants pairs', function () {
-    let pairSolution = new Solution("An old silent pond, A frog jumps into the pond, splash! Silence again.");
-    expect(pairSolution.countPair()).toEqual(14);
+  it('should be able to count vowels and consonants pairs for a word', function () {
+    let vowelConsonantSolution = new Solution("frog");
+    expect(vowelConsonantSolution.countPair()).toEqual(1);
   });
 
-  it('should count double vowels as one syllable', function () {
-    let doubleVowel = new Solution("An old silent pool, A frog jumps into the pool, splash! Silence again.");
-    expect(doubleVowel.countDoubleVowel()).toEqual(3);
+  it('should count double vowels for a word', function () {
+    let doubleVowels = new Solution("pool again.");
+    expect(doubleVowels.countDoubleVowel()).toEqual(2);
   });
 
   it('should count number of times a word ends with e', function () {
-    let endsWithIa= new Solution("An old silent pool, A frog jumps into the pool, splash! Inertia silence.");
-    expect(endsWithIa.countEndWithE()).toEqual(2);
+    let endsWithE= new Solution("the silence.");
+    expect(endsWithE.countEndWithE()).toEqual(2);
   });
 
   it('should count number of times a word ends with ia', function () {
-    let endsWithIa= new Solution("An old silent pool, A frog jumps into the pool, splash! Inertia silence.");
+    let endsWithIa= new Solution("Inertia");
     expect(endsWithIa.countEndWithIa()).toEqual(1);
   });
 
   it('should count number of times a word contains a prefix or suffix', function () {
-    let endsWithIa= new Solution("An old silent pool, A frog jumps into the pool, splash! Inertia silence.");
-    expect(endsWithIa.countPrePostFix()).toEqual(3);
+    let preSuffix= new Solution("An into");
+    expect(preSuffix.countPrePostFix()).toEqual(2);
   });
 
   it('should count number of times a word contains double consonants that makes a sound', function () {
-    let endsWithIa= new Solution("An old silent pool, A frog jumps into the pool, splash! Inertia silence.");
-    expect(endsWithIa.countConSound()).toEqual(2);
+    let endsWithConsSound = new Solution("the splash!");
+    expect(endsWithConsSound.countConSound()).toEqual(2);
   });
 
   it('should make sure a haiku has 17 syllables', function(){
