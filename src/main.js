64c2e2fe1962497ee53d2eae2d50070f1c88fcce
Ron Craig <ron.craig@comcast.net>
2018-10-04 23:11:22 -0700

    add show/hide jQuery, remove excess function outputs :tada:

--- src/main.js
+++ src/main.js
@@ -4,21 +4,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Solution } from './haiku-checker';
 
 $(document).ready(function() {
+  $('#haikus').hide();
+  $('#new-haiku-button').hide();
+  $('#haikus-text').hide();
+  $('#haiku-syllables').hide();
+  $('#haiku-details').hide();
+
   $('#haiku-form').submit(function(event) {
     event.preventDefault();
+    $('#haikus').show();
+    $('#new-haiku-button').show();
+    $('#haikus-text').show();
+    $('#haiku-syllables').show();
+    $('#haiku-details').show();
     let haiku = $('#haiku').val();
     let solution = new Solution(haiku.replace(/[^a-zA-Z ]/g, "").toLowerCase());
-    $('#haikus').append('<li>************ Haiku Results ************</li>');
-    $('#haikus').append('<li>Haiku text: ' + solution.haiku + '</li>');
-    $('#haikus').append('<li>Count vowels: ' + solution.countVowels() + '</li>');
-    $('#haikus').append('<li>Count words ending with [consonant]le: ' + solution.countEndWithLe() + '</li>');
-    $('#haikus').append('<li>Count double vowels, diphtongs or triphthongs: ' + solution.countDoubleVowel() + '</li>');
-    $('#haikus').append('<li>Count words ending with a silent e: ' + solution.countEndWithE() + '</li>');
-    $('#haikus').append('<li>Count words ending with ia: ' + solution.countEndWithIa() + '</li>');
-    $('#haikus').append('<li>Count 2 consonants resulting in sound within a word: ' + solution.countConSound() + '</li>');
-    $('#haikus').append('<li>Count 2 or more consonants next to each other: ' + solution.countNextConsonants() + '</li>');
-    $('#haikus').append('<li>************ Other Results ************</li>');
-    $('#haikus').append('<li>Count vowel-consonant pairs: ' + solution.countPair() + '</li>');
-    $('#haikus').append('<li>************ Seventeen (17) syllables? ************: ' + solution.haikuNumber() + '</li>');
+    $('#haikus-text').append('<b>' + solution.haiku + '</b>');
+    $('#haikus-syllables').append('<br>Poem syllables: ' + '<b>' + solution.haikuNumber() + ' syllables</b>');
+    $('#haikus-details').append('<li>Count vowels: ' + solution.countVowels() + '</li>');
+    $('#haikus-details').append('<li>Count words ending with [consonant]le: ' + solution.countEndWithLe() + '</li>');
+    $('#haikus-details').append('<li>Count double vowels, diphtongs or triphthongs: ' + solution.countDoubleVowel() + '</li>');
+    $('#haikus-details').append('<li>Count words ending with a silent e: ' + solution.countEndWithE() + '</li>');
+    $('#haikus-details').append('<li>Count words ending with ia: ' + solution.countEndWithIa() + '</li>');
+    $('#haikus-details').append('<br><h5>Sum of five numbers: ' + solution.haikuNumber() + '</h5>');
+  });
+
+  $('#new-haiku-button').click(function() {
+    event.preventDefault();
+    location.reload();
   });
 });
